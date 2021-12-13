a5d2d8667bc2ad278decf8ec61373d4e4a779a25
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-12-13 17:38:55 -0300

    Create Bootcamp class

---
+++ src/com/poo/domain/Bootcamp.java
@@ -0,0 +1,103 @@
+package com.poo.domain;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Bootcamp {
+    
+    private String name;
+    private String description;
+    private final LocalDate initialDate = LocalDate.now();
+    private final LocalDate finalDate = initialDate.plusDays(45);
+    private Set<Developer> devsSubscription = new HashSet<>();
+    private Set<Content>  contents = new LinkedHashSet<>();
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public LocalDate getInitialDate() {
+        return initialDate;
+    }
+    public LocalDate getFinalDate() {
+        return finalDate;
+    }
+    public Set<Developer> getDevsSubscription() {
+        return devsSubscription;
+    }
+    public void setDevsSubscription(Set<Developer> devsSubscription) {
+        this.devsSubscription = devsSubscription;
+    }
+    public Set<Content> getContents() {
+        return contents;
+    }
+    public void setContents(Set<Content> contents) {
+        this.contents = contents;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((contents == null) ? 0 : contents.hashCode());
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + ((devsSubscription == null) ? 0 : devsSubscription.hashCode());
+        result = prime * result + ((finalDate == null) ? 0 : finalDate.hashCode());
+        result = prime * result + ((initialDate == null) ? 0 : initialDate.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Bootcamp other = (Bootcamp) obj;
+        if (contents == null) {
+            if (other.contents != null)
+                return false;
+        } else if (!contents.equals(other.contents))
+            return false;
+        if (description == null) {
+            if (other.description != null)
+                return false;
+        } else if (!description.equals(other.description))
+            return false;
+        if (devsSubscription == null) {
+            if (other.devsSubscription != null)
+                return false;
+        } else if (!devsSubscription.equals(other.devsSubscription))
+            return false;
+        if (finalDate == null) {
+            if (other.finalDate != null)
+                return false;
+        } else if (!finalDate.equals(other.finalDate))
+            return false;
+        if (initialDate == null) {
+            if (other.initialDate != null)
+                return false;
+        } else if (!initialDate.equals(other.initialDate))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+    
+    
+}
