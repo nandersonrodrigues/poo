c443116fa7c33c6cc0d08e57a3572e1ffe1d1a06
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-12-13 17:23:48 -0300

    Create main class

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import com.poo.domain.Content;
+import com.poo.domain.Course;
+import com.poo.domain.Mentoring;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        Content course1 = new Course();
+        Content mentoring = new Mentoring();   
+    }
+}
+
